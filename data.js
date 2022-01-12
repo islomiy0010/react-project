8c5691c087be434c295ae07814a4c374d736aefe
islomiy0010 <84896005+islomiy0010@users.noreply.github.com>
2022-01-12 10:14:00 +0500

    Add files via upload

---
+++ data.js
@@ -0,0 +1,32 @@
+export const products = [
+  {
+    id: 1,
+    img: "https://olcha.uz/image/original/products/opFD9qkiqL3mP5jp9DkCAlbmSOxU5vmNjg2qw8pFHqZ1HEKrP6NChZmywwl2.jpeg",
+    link: "https://olcha.uz/",
+  },
+  {
+    id: 2,
+    img: "https://olcha.uz/image/original/products/Vf9M2Aw4xhc29Rn0XvIAoXGX8eWSmG6IrhilMVnk9qqxYgWY2hCjdDrZD9am.jpeg",
+    link: "https://olcha.uz/",
+  },
+  {
+    id: 3,
+    img: "https://olcha.uz/image/original/products/45pMa4sCpOrH1sDHXjdtgAQPa3MrW3fQvA25EEOPqAYutmi9fixBt2F861Wg.jpeg",
+    link: "https://olcha.uz/",
+  },
+  {
+    id: 4,
+    img: "https://olcha.uz/image/original/products/K8P4jlQyKEpchlIBrv1nSd9hfV2ewmdtLyDTgCCcqqwYAxKwEVnSPCe3Y1lh.jpeg",
+    link: "https://olcha.uz/",
+  },
+  {
+    id: 5,
+    img: "https://olcha.uz/image/original/products/qbFk7hJOLfCemAbIfB54dRbF0CNYYyEiryq1E4rVtNhMPillDMlkodkcOqkk.jpeg",
+    link: "https://olcha.uz/",
+  },
+  {
+    id: 6,
+    img: "https://olcha.uz/image/original/products/diIXorQ8EPifZMNdPOBYWvf6DPRXQ60e2gIYnl3J7RkLZEruOlT1JVSPR4lh.jpeg",
+    link: "https://olcha.uz/",
+  },
+];
